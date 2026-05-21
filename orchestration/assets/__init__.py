c639e726f7c641fa13dbda43000408bd543865dc
vsrirama <vivek.srirama@gmail.com>
2026-05-21 14:22:28 -0700

    Adding thermal models into assets

--- orchestration/assets/__init__.py
+++ orchestration/assets/__init__.py
@@ -1,5 +1,6 @@
 from orchestration.assets.occupancy import occupancy_model
+from orchestration.assets.thermal import thermal_em_model, thermal_etotal_model
 
-all_assets = [occupancy_model]
+all_assets = [occupancy_model, thermal_em_model, thermal_etotal_model]
 
-__all__ = ["all_assets", "occupancy_model"]
+__all__ = ["all_assets", "occupancy_model", "thermal_em_model", "thermal_etotal_model"]
